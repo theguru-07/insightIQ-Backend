a9f52b731fdb6f20bf50218bbfeca18f971522ec
Malhar Prasad Ray <malharprasadray@gmail.com>
2026-02-05 22:55:20 +0530

    api issue fixed

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -9,7 +9,10 @@ const authMiddleware = (req, res, next) => {
       return res.status(401).json({ message: "No token provided" });
     }
 
-    const token = authHeader.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+  return res.status(401).json({ message: "Unauthorized" });
+}
 
     // 2. Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
--- src/routes/competitor.routes.js
+++ src/routes/competitor.routes.js
@@ -6,7 +6,7 @@ import {
   updateCompetitor,
   deleteCompetitor,
 } from "../controllers/competitor.controller.js";
-import authMiddleware from "../middlewares/auth.middleware.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
